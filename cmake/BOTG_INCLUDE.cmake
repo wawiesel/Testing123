983df7f15a8180b760f40954b42bc0d1c6b9bc4d
William A. Wieselquist <ww5@ornl.gov>
2017-02-02 16:40:52 -0500

    [removed] external BootsOnTheGround subtree in favor of boostrapping

---
+++ cmake/BOTG_INCLUDE.cmake
@@ -0,0 +1,39 @@
+#This function is special and needs to be here so it can be part of a
+#Bootstrapping operation.
+MACRO( BOTG_DownloadExternalProjects external_projects )
+    FOREACH( ep ${external_projects} )
+        MESSAGE( STATUS "loading external project=${ep}...")
+        SET( dest "${CMAKE_BINARY_DIR}/external/download/${ep}" )
+        CONFIGURE_FILE("${CMAKE_SOURCE_DIR}/external/${ep}.in" "${dest}/CMakeLists.txt" )
+        EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" . WORKING_DIRECTORY "${dest}")
+        EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} --build . WORKING_DIRECTORY "${dest}")
+    ENDFOREACH()
+ENDMACRO()
+
+# Use cached value.
+IF( DEFINED BOTG_SOURCE_DIR )
+
+    IF( EXISTS "${BOTG_SOURCE_DIR}" )
+        MESSAGE( STATUS "[BootsOnTheGround] using cached BOTG_SOURCE_DIR=${BOTG_SOURCE_DIR} ... ")
+    ELSE()
+        MESSAGE( ERROR "[BootsOnTheGround] cached BOTG_SOURCE_DIR=${BOTG_SOURCE_DIR} does not exist!")
+    ENDIF()
+
+# Set the BootsOnTheGround source directory!
+ELSE()
+
+    SET(BOTG_SOURCE_DIR "${CMAKE_SOURCE_DIR}/external/BootsOnTheGround" CACHE PATH INTERNAL)
+
+    IF( EXISTS "${BOTG_SOURCE_DIR}" )
+        MESSAGE( STATUS "[BootsOnTheGround] using BOTG_SOURCE_DIR=${BOTG_SOURCE_DIR} ... ")
+    ELSE()
+        MESSAGE( STATUS "[BootsOnTheGround] bootstrapping in...")
+        BOTG_DownloadExternalProjects( BootsOnTheGround )
+    ENDIF()
+
+ENDIF()
+
+# Includes all the "BootsOnTheGround" (BOTG) functions.
+INCLUDE( "${BOTG_SOURCE_DIR}/cmake/BOTG.cmake" )
+
+
--- external/BootsOnTheGround/external/TriBITS/test/ci_support/ExtraReposListExisting_4.cmake
+++
@@ -1,6 +0,0 @@
-TRIBITS_PROJECT_DEFINE_EXTRA_REPOSITORIES(
-  preRepoOnePackage     ""  GIT  url1:/git/preRepoOnePackage         PRE  Continuous
-  preCopyrightTrilinos  ""  GIT  url1.com:/git/preCopyrightTrilinos  ""   Continuous
-  extraTrilinosRepo     ""  GIT  usr2.com:/git/extraTrilinosRepo     ""   Nightly
-  ExtraTeuchosRepo  packages/teuchos/extrastuff GIT  url3.com:/ExtraStuff  NOPACKAGES  Continuous
-  )
